c8babd5cdd702e3b5e033beaa7b236952c158103
darshan009 <darshansapaliga009@gmail.com>
2015-11-19 01:08:12 +0530

    updated onclick and players

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var deck = function(){
   {
     for(var j = 0; j< this.names.length;j++){
       cards.push(new card(j+1, this.names[j], this.suits[i]));
-      console.log(cards);
+      
     }
       
   }
@@ -23,16 +23,70 @@ var deck = function(){
   return cards;
 };
 
+//shuffling cards
+function stackShuffle(number) {
+  var deck = number;
+  var i, j, k, temp;
+
+  for (i = 0; i < deck.length; i++)
+  {
+      k = Math.floor(Math.random() * (i+1));
+      temp = deck[i] ;
+      deck[i] = deck[k];
+      deck[k] = temp;
+    }
+  return deck;
+}
+
+//array of players
+var players =[];
+players[0]= new player(prompt("Enter your player1 name?"));
+players[1] = new player(prompt("Enter your player2 name?"));
+
+
+cardCounter = 0;
+playerCounter = 0;
+
+//player temp fn
+var player= function(name){
+    this.name = name;
+}
+
 //Creating a deck object
 var myDeck = new deck();
 
 window.onload = function(){
 
+   stackShuffle(myDeck);
   //Loop for all 52 cards in the deck
   for(var i = 0; i< myDeck.length; i++)
   {
     div = document.createElement('div');
     div.className = 'card';
+    div.addEventListener('click', function(event){
+
+      if(cardCounter == 2){
+        var flippCard = document.getElementsByClassName("show");
+        cardCounter = 1;
+      }
+      else if(cardCounter == 1){
+        //queryselector???
+        currentValue = ;
+        previousValue = ;
+        if(currentValue == previousValue){
+            players[playerCounter].score = players[playerCounter].score + 10;
+        }
+      }
+      else
+        cardCounter++;
+
+
+    });
+
+
+
+
+
     var ascii_char;
     if(myDeck[i].suit =='Diamonds'){
       ascii_char = '&diams;';
@@ -44,6 +98,14 @@ window.onload = function(){
     div.innerHTML ='<span class="number">'+ myDeck[i].name +'</span><span class="suit">' + ascii_char +'</span>';
     document.body.appendChild(div);
   }
-};
 
+
+  messageDiv = document.createElement('div');
+  messageDiv.setAttribute('id','messages');
+  messageDiv.innerHTML= players[0].name +', this is your turn';
+  document.body.appendChild(messageDiv);
+
+
+
+};
 
